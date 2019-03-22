4e068b63f25411d10096a12d924f658cd89e7545
Isaac Hall <isaacdehall@gmail.com>
2019-03-22 15:18:21 -0700

    separate logic, create first draft of UI results

--- src/backEnd.js
+++ src/backEnd.js
@@ -24,6 +24,59 @@ export class DocTalker{
     })
   }
 }
+export class DocOp {
+  constructor() {
+
+  }
+  querySearch(firstName, lastName, query) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://api.betterdoctor.com/2016-03-01/doctors?first_name=${firstName}&last_name=${lastName}&query=${query}&location=or-portland&user_location=45.512%2C%20122.658&skip=0&limit=10&user_key=${process.env.apiKey}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          // let legible = JSON.parse(request.response);
+          // console.log(legible.data);
+
+          resolve(request.response);
+
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
+//   let sQ = new DocTalker;
+//   // let searchQuery = new DocTalker($("#firstNameInput, #lastNameInput, 'addy', 'digits', 'website', 'open'").val())
+//   let firstName = $('#firstNameInput').val();
+//   let lastName = $('#lastNameInput').val();
+//   let query = $('#medIssueInput').val();
+//   console.log(firstName);
+//   let name = sQ.nameMaker(firstName, lastName);
+//   console.log (name);
+//   // const city = $('#location').val();
+//   $('input').val("");
+//
+//   let request = new XMLHttpRequest();
+//   const url = `https://api.betterdoctor.com/2016-03-01/doctors?first_name=${firstName}&last_name=${lastName}&query=${query}&location=or-portland&user_location=45.512%2C%20122.658&skip=0&limit=10&user_key=${process.env.apiKey}`;
+//
+//   request.onreadystatechange = function() {
+//     if (this.readyState === 4 && this.status === 200) {
+//       const response = JSON.parse(this.responseText);
+//       console.log(response);
+//       // sQ.eachInArr(response.data);
+//       console.log(response.data[0].profile.bio);
+//       console.log(response.data[1].profile.bio);
+//       console.log(response.data[2].profile.bio);
+//       console.log(response.data[3].profile.bio);
+//       return response;
+//     }
+//   }
+//   request.open("GET", url, true);
+//   request.send();
+// }
 
 
 
--- src/main.js
+++ src/main.js
@@ -5,47 +5,63 @@ import './sass/styles.scss';
 import $ from 'jquery';
 
 import {DocTalker} from "./backEnd.js";
+import {DocOp} from "./backEnd.js";
 
 $(document).ready(function() {
 
   $("#ageForm").submit(function(event) {
     event.preventDefault();
     let sQ = new DocTalker;
-    // let searchQuery = new DocTalker($("#firstNameInput, #lastNameInput, 'addy', 'digits', 'website', 'open'").val())
+    let sQuery = new DocOp;
     let firstName = $('#firstNameInput').val();
     let lastName = $('#lastNameInput').val();
     let query = $('#medIssueInput').val();
-    console.log(firstName);
-    let name = sQ.nameMaker(firstName, lastName);
-    console.log (name);
-    // const city = $('#location').val();
+    const docs = [];
+    const findDocs = sQuery.querySearch(firstName, lastName, query);
+    console.log(findDocs);
+    // let name = sQ.nameMaker(firstName, lastName);
+    // console.log (name);
     $('input').val("");
 
-    let request = new XMLHttpRequest();
-    const url = `https://api.betterdoctor.com/2016-03-01/doctors?first_name=${firstName}&last_name=${lastName}&query=${query}&location=or-portland&user_location=45.512%2C%20122.658&skip=0&limit=10&user_key=${process.env.apiKey}`;
+    findDocs.then(function(response) {
+      let legible = JSON.parse(response);
+      console.log(legible.data[1].profile.bio);
+      legible.data.forEach(function(index){
 
-    // const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.apiKey}`;
+        console.log(index.profile)
+        docs.push(index.profile.bio);
+        $(".result").append(`${index.profile.first_name} ${index.profile.last_name}<br>${index.profile.bio} <br><br>`);
 
-    request.onreadystatechange = function() {
-      if (this.readyState === 4 && this.status === 200) {
-        const response = JSON.parse(this.responseText);
-        console.log(response);
-        // console.log(response.meta.total);
-        // console.log(response.data[0].profile.bio);
+      });
+      // console.log(docs);
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+    });
+    // console.log(cardsArr);
 
-        // sQ.eachInArr(response.data);
-        console.log(response.data[0].profile.bio);
-        console.log(response.data[1].profile.bio);
-        console.log(response.data[2].profile.bio);
-        console.log(response.data[3].profile.bio);
-        return response;
-      }
-    }
 
-    //create seperation with promise 
 
-    request.open("GET", url, true);
-    request.send();
+
+
+    // let request = new XMLHttpRequest();
+    // const url = `https://api.betterdoctor.com/2016-03-01/doctors?first_name=${firstName}&last_name=${lastName}&query=${query}&location=or-portland&user_location=45.512%2C%20122.658&skip=0&limit=10&user_key=${process.env.apiKey}`;
+    //
+    // request.onreadystatechange = function() {
+    //   if (this.readyState === 4 && this.status === 200) {
+    //     const response = JSON.parse(this.responseText);
+    //     console.log(response);
+    //     // sQ.eachInArr(response.data);
+    //     console.log(response.data[0].profile.bio);
+    //     console.log(response.data[1].profile.bio);
+    //     console.log(response.data[2].profile.bio);
+    //     console.log(response.data[3].profile.bio);
+    //     return response;
+    //   }
+    // }
+    // request.open("GET", url, true);
+    // request.send();
+
+    // ui show
     // console.log(response);
     // const getElements = function(response) {
     //   $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
