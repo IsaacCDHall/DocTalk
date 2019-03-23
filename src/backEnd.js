d980156caaa911db7390add314786ab81495f012
Isaac Hall <isaacdehall@gmail.com>
2019-03-22 17:12:44 -0700

    fix phone number placement bug

--- src/backEnd.js
+++ src/backEnd.js
@@ -1,29 +1,3 @@
-export class DocTalker{
-  constructor(firstName, lastName, addy, digits, website, open){
-    this.firstName=firstName;
-    this.lastName=lastName;
-    this.addy=addy;
-    this.digits=digits;
-    this.website=website;
-    this.open=open;
-  }
-
-  bustedNameMaker(){
-    const name= `${this.firstName} ` + this.lastName;
-    console.log(name)
-    return name;
-  }
-  nameMaker(firstName, lastName){
-    const name= `${firstName} ${lastName}`;
-    console.log(name)
-    return name;
-  }
-  eachInArr(arr, element){
-    arr.forEach(function(element){
-      console.log(element)
-    })
-  }
-}
 export class DocOp {
   constructor() {
 
@@ -44,6 +18,32 @@ export class DocOp {
     });
   }
 }
+// export class DocTalker{
+//   constructor(firstName, lastName, addy, digits, website, open){
+//     this.firstName=firstName;
+//     this.lastName=lastName;
+//     this.addy=addy;
+//     this.digits=digits;
+//     this.website=website;
+//     this.open=open;
+//   }
+//
+//   bustedNameMaker(){
+//     const name= `${this.firstName} ` + this.lastName;
+//     console.log(name)
+//     return name;
+//   }
+//   nameMaker(firstName, lastName){
+//     const name= `${firstName} ${lastName}`;
+//     console.log(name)
+//     return name;
+//   }
+//   eachInArr(arr, element){
+//     arr.forEach(function(element){
+//       console.log(element)
+//     })
+//   }
+// }
 //   let sQ = new DocTalker;
 //   // let searchQuery = new DocTalker($("#firstNameInput, #lastNameInput, 'addy', 'digits', 'website', 'open'").val())
 //   let firstName = $('#firstNameInput').val();
--- src/main.js
+++ src/main.js
@@ -3,21 +3,18 @@ import 'bootstrap/scss/bootstrap.scss';
 import './sass/styles.scss';
 // import './backEnd.js';
 import $ from 'jquery';
-
-import {DocTalker} from "./backEnd.js";
 import {DocOp} from "./backEnd.js";
 
 $(document).ready(function() {
 
   $("#ageForm").submit(function(event) {
     event.preventDefault();
-    let sQ = new DocTalker;
     let sQuery = new DocOp;
     let firstName = $('#firstNameInput').val();
     let lastName = $('#lastNameInput').val();
     let query = $('#medIssueInput').val();
     const docsBio = [];
-    const docsContact = [];
+    // const docsContact = [];
     const findDocs = sQuery.querySearch(firstName, lastName, query);
     // let name = sQ.nameMaker(firstName, lastName);
     // console.log (name);
@@ -30,23 +27,18 @@ $(document).ready(function() {
       }else {
         // console.log(legible.data[1].profile.bio);
         legible.data.forEach(function(index){
-          console.log(index);
           docsBio.push(index.profile.bio);
           $(".result").append(`<h3>${index.profile.first_name} ${index.profile.last_name}</h3>`);
           index.practices.forEach(function(locations){
-            console.log(locations.website);
             if (locations.accepts_new_patients == true) {
-              console.log(locations.website);
-
-              $(".result").append(`${locations.visit_address.street}, ${locations.visit_address.city}, ${locations.visit_address.state}  ${locations.visit_address.zip} <br>${locations.phones[0].number}`);
+              $(".result").append(`${locations.visit_address.street}, ${locations.visit_address.city}, ${locations.visit_address.state}  ${locations.visit_address.zip} <br>${locations.phones[0].number}<br>`);
             }else {
               $(".result").append("This doctor is not currently accepting new patients");
             }
             if (locations.website !=undefined) {
               $(".result").append(`<br>${locations.website}<br>`);
-            } 
+            }
           });
-          console.log(index.practices.website);
 
           $(".result").append(`<br><i>${index.profile.bio}</i> <br><br>`)
           // console.log(index.practices)
