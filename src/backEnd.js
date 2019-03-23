4fd77b93aab6c7550c7b5ec5515400030e60f1a1
Isaac Hall <isaacdehall@gmail.com>
2019-03-22 17:04:57 -0700

    add website, if available

--- src/backEnd.js
+++ src/backEnd.js
@@ -34,11 +34,7 @@ export class DocOp {
       const url = `https://api.betterdoctor.com/2016-03-01/doctors?first_name=${firstName}&last_name=${lastName}&query=${query}&location=or-portland&user_location=45.512%2C%20122.658&skip=0&limit=10&user_key=${process.env.apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
-          // let legible = JSON.parse(request.response);
-          // console.log(legible.data);
-
           resolve(request.response);
-
         } else {
           reject(Error(request.statusText));
         }
--- src/main.js
+++ src/main.js
@@ -25,70 +25,37 @@ $(document).ready(function() {
 
     findDocs.then(function(response) {
       let legible = JSON.parse(response);
-      console.log(legible.meta.count);
       if (legible.meta.count === 0) {
         $(".result").append("There are no results for this search");
       }else {
         // console.log(legible.data[1].profile.bio);
         legible.data.forEach(function(index){
-          // console.log(index);
+          console.log(index);
           docsBio.push(index.profile.bio);
-          $(".result").append(`${index.profile.first_name} ${index.profile.last_name}<br>`);
-          console.log(index.practices);
+          $(".result").append(`<h3>${index.profile.first_name} ${index.profile.last_name}</h3>`);
           index.practices.forEach(function(locations){
-            console.log(locations.accepts_new_patients);
+            console.log(locations.website);
             if (locations.accepts_new_patients == true) {
-              console.log("true");
-              console.log(locations.visit_address);
+              console.log(locations.website);
 
-              $(".result").append(`${locations.visit_address.street}, ${locations.visit_address.city}, ${locations.visit_address.state}  ${locations.visit_address.zip} <br> `);
+              $(".result").append(`${locations.visit_address.street}, ${locations.visit_address.city}, ${locations.visit_address.state}  ${locations.visit_address.zip} <br>${locations.phones[0].number}`);
             }else {
-
-              console.log("false")
+              $(".result").append("This doctor is not currently accepting new patients");
             }
+            if (locations.website !=undefined) {
+              $(".result").append(`<br>${locations.website}<br>`);
+            } 
           });
+          console.log(index.practices.website);
 
-
-          $(".result").append(`${index.profile.bio} <br><br>`);
-          console.log(index.practices);
+          $(".result").append(`<br><i>${index.profile.bio}</i> <br><br>`)
+          // console.log(index.practices)
 
         });
-
       }
-      // console.log(docsBio);
     }, function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error.message}`);
     });
-
-
-
-
-
-
-    // let request = new XMLHttpRequest();
-    // const url = `https://api.betterdoctor.com/2016-03-01/doctors?first_name=${firstName}&last_name=${lastName}&query=${query}&location=or-portland&user_location=45.512%2C%20122.658&skip=0&limit=10&user_key=${process.env.apiKey}`;
-    //
-    // request.onreadystatechange = function() {
-    //   if (this.readyState === 4 && this.status === 200) {
-    //     const response = JSON.parse(this.responseText);
-    //     console.log(response);
-    //     // sQ.eachInArr(response.data);
-    //     console.log(response.data[0].profile.bio);
-    //     console.log(response.data[1].profile.bio);
-    //     console.log(response.data[2].profile.bio);
-    //     console.log(response.data[3].profile.bio);
-    //     return response;
-    //   }
-    // }
-    // request.open("GET", url, true);
-    // request.send();
-
-    // ui show
-    // console.log(response);
-    // const getElements = function(response) {
-    //   $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-    //   $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
-    // }
    $('.result').show();
   });
 });
